fbe7c3f3573952d569c764e76ab0c99ddec4a929
Luckychuan <luckychuan96@163.com>
2017-05-02 17:38:19 +0800

    完成首页banner

--- app/src/main/java/com/example/luckychuan/myzhihudaily/TestActivity.java
+++ app/src/main/java/com/example/luckychuan/myzhihudaily/TestActivity.java
@@ -2,12 +2,13 @@ package com.example.luckychuan.myzhihudaily;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.support.v4.view.ViewPager;
 
 import com.example.luckychuan.myzhihudaily.adapter.TopStoryAdapter;
 import com.example.luckychuan.myzhihudaily.bean.LatestData;
 import com.example.luckychuan.myzhihudaily.presenter.GetLatestDataPresenter;
 import com.example.luckychuan.myzhihudaily.view.LatestDataView;
+import com.example.luckychuan.myzhihudaily.widget.AutoSlideViewPager;
+import com.viewpagerindicator.CirclePageIndicator;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,14 +21,18 @@ public class TestActivity extends Activity implements LatestDataView {
     private List<LatestData.TopStory> mTopStories = new ArrayList<>();
     private GetLatestDataPresenter mLDPresenter;
     private TopStoryAdapter mAdapter;
+    private CirclePageIndicator mIndicator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.top_story_holder_layout);
-        ViewPager viewPager = (ViewPager) findViewById(R.id.viewPager);
+
+        AutoSlideViewPager viewPager = (AutoSlideViewPager) findViewById(R.id.viewPager);
         mAdapter = new TopStoryAdapter(mTopStories);
         viewPager.setAdapter(mAdapter);
+        mIndicator = (CirclePageIndicator) findViewById(R.id.pagerIndicator);
+        mIndicator.setViewPager(viewPager);
 
         mLDPresenter = new GetLatestDataPresenter(this);
         mLDPresenter.attach(this);
--- app/src/main/java/com/example/luckychuan/myzhihudaily/widget/AutoSlideViewPager.java
+++ app/src/main/java/com/example/luckychuan/myzhihudaily/widget/AutoSlideViewPager.java
@@ -4,15 +4,45 @@ import android.content.Context;
 import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
 
+import java.util.concurrent.TimeUnit;
+
+import rx.Observable;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action1;
+
 /**
  * 自动滑动的viewpager
  */
 public class AutoSlideViewPager extends ViewPager {
+
     public AutoSlideViewPager(Context context) {
         super(context);
     }
 
     public AutoSlideViewPager(Context context, AttributeSet attrs) {
         super(context, attrs);
+    }
+
+
+    private void startGlide() {
+        Observable.interval(5, 5, TimeUnit.SECONDS)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Action1<Long>() {
+                    @Override
+                    public void call(Long aLong) {
+                        if (getCurrentItem() == getAdapter().getCount() - 1) {
+                            setCurrentItem(0);
+                        } else {
+                            setCurrentItem(getCurrentItem() + 1);
+                        }
+
+                    }
+                });
+    }
+
+    @Override
+    protected void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        startGlide();
     }
 }
