d9ff19f671a906047cf68ab399250aacc6094d19
Luckychuan <luckychuan96@163.com>
2017-05-18 15:17:44 +0800

    添加功能：收藏列表

---
+++ app/src/main/java/com/example/luckychuan/myzhihudaily/adapter/FavoriteRecyclerAdapter.java
@@ -0,0 +1,56 @@
+package com.example.luckychuan.myzhihudaily.adapter;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import com.example.luckychuan.myzhihudaily.R;
+import com.example.luckychuan.myzhihudaily.adapter.viewholder.StoryViewHolder;
+import com.example.luckychuan.myzhihudaily.bean.Story;
+import com.example.luckychuan.myzhihudaily.bean.StoryLite;
+import com.example.luckychuan.myzhihudaily.ui.StoryActivity;
+
+import java.util.List;
+
+/**
+ * Created by Luckychuan on 2017/5/18.
+ */
+
+public class FavoriteRecyclerAdapter extends RecyclerView.Adapter<StoryViewHolder> {
+
+    private List<Story> mList;
+
+    public FavoriteRecyclerAdapter(List<Story> list) {
+        mList = list;
+    }
+
+
+    @Override
+    public StoryViewHolder onCreateViewHolder(final ViewGroup parent, int viewType) {
+        StoryViewHolder viewHolder = new StoryViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.story_holder_layout, parent, false));
+        viewHolder.setOnClickListener(new StoryViewHolder.OnItemClickListener() {
+            @Override
+            public void OnItemClick(int position) {
+                Story story = ((Story) mList.get(position));
+                Context context = parent.getContext();
+                Intent intent = new Intent(context, StoryActivity.class);
+                intent.putExtra("story", story);
+                context.startActivity(intent);
+            }
+        });
+
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(StoryViewHolder holder, int position) {
+        holder.bindViewHolder(mList.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mList.size();
+    }
+}
--- app/src/main/java/com/example/luckychuan/myzhihudaily/ui/FavoriteFragment.java
+++ app/src/main/java/com/example/luckychuan/myzhihudaily/ui/FavoriteFragment.java
@@ -2,28 +2,76 @@ package com.example.luckychuan.myzhihudaily.ui;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.luckychuan.myzhihudaily.R;
+import com.example.luckychuan.myzhihudaily.adapter.FavoriteRecyclerAdapter;
+import com.example.luckychuan.myzhihudaily.bean.Story;
+import com.example.luckychuan.myzhihudaily.bean.StoryLite;
+
+import org.litepal.crud.DataSupport;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Luckychuan on 2017/5/17.
  */
 
-public class FavoriteFragment extends BaseFragment  {
+public class FavoriteFragment extends BaseFragment {
 
     private OnTitleChangeListener mListener;
-    
+    private List<Story> mList;
+    private FavoriteRecyclerAdapter mAdapter;
 
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_favorite,container,false);
+        return inflater.inflate(R.layout.fragment_favorite, container, false);
     }
 
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        mList = new ArrayList<>();
+       initData();
+
+        if (mListener != null) {
+            mListener.changeToolbarTitle(mList.size() + "条收藏");
+        }
+
+        RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.recycler_favorite);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        mAdapter = new FavoriteRecyclerAdapter(mList);
+        recyclerView.setAdapter(mAdapter);
+
+
+    }
+
+
+    /**
+     * /从数据库中获取数据
+     */
+    private void initData() {
+        mList.clear();
+        List<StoryLite> list = DataSupport.findAll(StoryLite.class);
+        for (StoryLite storyLite : list) {
+
+            Story story = new Story();
+            story.setId(storyLite.getStoryId());
+            story.setTitle(storyLite.getTitle());
+            story.setImageUrl(new String[]{storyLite.getImageUrl()});
+            story.setMultiPic(storyLite.isMultiPic());
+
+            mList.add(story);
+        }
+    }
 
     @Override
     public void setTitleChangeListener(OnTitleChangeListener listener) {
@@ -33,13 +81,23 @@ public class FavoriteFragment extends BaseFragment  {
     @Override
     public void onHiddenChanged(boolean hidden) {
         super.onHiddenChanged(hidden);
-        if(!hidden){
-            if(mListener !=null){
-                // TODO: 2017/5/18  
-                mListener.changeToolbarTitle("条收藏");
-            }
+        if (!hidden) {
+           refreshData();
         }
-        
-        
     }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        refreshData();
+    }
+
+    public void refreshData(){
+        initData();
+        mAdapter.notifyDataSetChanged();
+        if (mListener != null) {
+            mListener.changeToolbarTitle(mList.size() + "条收藏");
+        }
+    }
+
 }
